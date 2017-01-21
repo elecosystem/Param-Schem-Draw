df440345cc672866ddab9f3e7178c5540bb2d326
Pedro Martins <martinspedro@ua.pt>
2017-01-21 11:21:49 +0000

    Working | Needs more tests

--- ParamSchemDraw.py
+++ ParamSchemDraw.py
@@ -10,75 +10,7 @@
 
 from numbers import Number, Complex
 from decimal import Decimal
-from math import floor, log10, pow
-
-def frexp10(x):
-    """
-    Return mantissa and exponent (base 10), similar to base-2 frexp()
-    :param x: floating point number
-    :return: tuple (mantissa, exponent)
-    """
-    exp = floor(log10(x))
-    return x/10**exp, exp
-
-
-def eng_notate(x, d=3, suff=True, neg_ok=False):
-    # Practical component suffixes
-    _SUFFIX = ["p", "n", "u", "m", "", "k", "M", "G"]
-    # Offset to unit multiplier (no suffix)
-
-    _UNIT_OFFSET = 4
-
-    sign = ""
-    if neg_ok and x < 0.0:
-        sign = "-"
-        x = -x
-    #if x < 0.0 or not isfinite(x):
-    #    return "RangeErr"
-    if x == 0:
-        return "0.0"
-    # Normalize the number and round to get d significant digits
-    mant, exp = frexp10(x)
-    r = round(mant,d-1)
-    # Convert back to original scale
-    x = r * pow(10.0, exp)
-    # Get integer exponent to group by factors of 1000
-    p = int(floor(log10(x)))
-    p3 = p // 3
-    # Get root value string
-    value = x / pow(10.0, 3*p3)
-    numStr = "{:f}".format(value)
-    # Slice to length, avoid trailing "."
-    if numStr[d] != ".":
-        numStr = numStr[0:d+1]
-    else:
-        numStr = numStr[0:d]
-    # Prepend '-' if necessary
-    numStr = sign + numStr
-    if suff:
-        # Append units suffix
-        p3i = p3 + _UNIT_OFFSET
-        if p3i < 0:
-            # Smaller than lowest unit
-            return "<1{}".format(_SUFFIX[0])
-        if p3i > len(_SUFFIX)-1:
-            # Larger than largest unit
-            return ">999{}".format(_SUFFIX[-1])
-        else:
-            s = _SUFFIX[p3i]
-            return "{}{}".format(numStr, s)
-    else:
-        # No suffix, return floating point string
-        if p3 != 0:
-            return "{}e{:d}".format(numStr, 3*p3)
-        else:
-            return "{}".format(numStr)
-
-
-def round2Precision(x, p):
-    n = floor(log10(x)) + 1 - p
-    print n
-    return round(10 ** (-n) * x) * 10 ** n
+from math import floor, log10
 
 # Classes to work with eletrical components
 class electricComponent(object):
@@ -87,71 +19,58 @@ class electricComponent(object):
         '''
             Formats a number to engineering notation with p significant digits
         '''
-        assert value != 0
-        # Practical component suffixes
-        _SUFFIX = ["p", "n", "u", "m", "", "k", "M", "G"]
-        # Offset to unit multiplier (no suffix)
 
+        # Engineering units prefixes and offset to unitary prefix
+        _PREFIX = ('$p$', '$n$', '$\mu$', '$m$', "", '$K$', '$M$', '$G$')
         _UNIT_OFFSET = 4
 
-        # For negative cases
+        # Handling negative numbers and zero
         sign = ""
         if value < 0:
             sign = "-"
             value = -value
+        elif value == 0:
+            return '0'
 
         # Exponent and mantissa
         exp = floor(log10(value))
         mant = value / 10 ** exp
 
-        # Get significant digits
-        trunc = round(mant, p-1)
-        truncPrec = trunc * 10 ** exp
+        # Round number to significant number of digits
+        mantPrecise = round(mant, p-1)
+        valuePrecise = mantPrecise * 10 ** exp
 
+        # Convert number to engineer Notation
         engExp = int(exp) // 3
-        engValue = truncPrec / (10 ** (3 * engExp))
-        engStr = "{:f}".format(engValue)
+        mantEng = valuePrecise / (10 ** (3 * engExp))
+        mantEngStr = "{:f}".format(mantEng)
 
-        # Slice to length, avoid trailing "."
-        if engStr[p] != ".":
-            engStr = engStr[0:p+1]
-        else:
-            engStr = engStr[0:p]
-        # Prepend '-' if necessary
-        engStr = sign + engStr
+        '''
+            Text Formatting
+                #.00***, ##.0*** -> remove the decimal dot & all zeros after decimal dot
+                #.#0... -> remove the zeros after the last nonzero number after the decimal dot
+                ###.    -> remove the decimal dot
+                ##.     -> add one  more digit
+                #.#
+        '''
+        if mantEngStr[p] != ".":
+            if mantEngStr[p] == '0':
+                if mantEngStr[p-1] == '0':
+                    mantEngStr = mantEngStr[0:p-2]
+                else:
+                    mantEngStr = mantEngStr[0:p-1]
+            # if last digit is not a point, means it has a decimal point in the string
+            # therefore it has one less digit
+            mantEngStr = mantEngStr[0:p+1]
+        elif mantEngStr[p+1] == '0':
+            mantEngStr = mantEngStr[0:p]
 
         engPrefix = engExp + _UNIT_OFFSET
-        #if engPrefix < 0:
-        #    # Smaller than lowest unit
-        #    return "<1{}".format(_SUFFIX[0])
-        #if p3i > len(_SUFFIX)-1:
-            # Larger than largest unit
-        #    return ">999{}".format(_SUFFIX[-1])
-        #else:
-        s = _SUFFIX[engPrefix]
-        return "{}{}{}".format(engStr, s, units)
+        if (engPrefix < 0) or (engExp > len(_PREFIX)-1):
+            # Smaller than lowest unit or higher than higher unit
+            raise ValueOutsideReasonableBounds
 
-
-    '''
-            return eng_notate(value) + units
-
-
-
-            value = round2Precision(value, 3)
-            if( value >= 1 * 10 ** 6 ):
-                return str( (value % (1 * 10 ** 6)) * 10 ** -6 + (value / (1 * 10 ** 6)) ) + '$M$'  + units
-            elif( value >= 1 * 10 ** 3 ):
-                return str( (value % (1 * 10 ** 3)) * 10 ** -3 + (value / (1 * 10 ** 3)) ) + '$K$' + units
-            elif( value >= 1 ):
-                return str(value) + units
-            elif( value >= 1 * 10 ** -3):
-                 return str( value / (1 * 10 ** -3) ) + '$m$' + units
-            elif( value >= 1 * 10 ** -6):
-                 return str( value / (1 * 10 ** -6) ) + '$\mu$' + units
-            else:
-                raise ValueOutsideReasonableBounds
-    '''
-
+        return "{}{}{}{}".format(sign, mantEngStr, _PREFIX[engPrefix], units)
 
 class resistor(electricComponent):
     def __init__(self, value, label= ""):
@@ -171,8 +90,7 @@ class resistor(electricComponent):
 
     @property
     def value(self):
-        return eng_notate(self._value)
-        #return electricComponent.enginnerNotation(self._value, '$\Omega$')
+        return electricComponent.enginnerNotation(self._value, resistor.__UNIT)
 
     @property
     def label(self):
@@ -186,8 +104,10 @@ class resistor(electricComponent):
     def schem(self, schematic):
         self._schem = schematic
 
+    __UNIT = '$\Omega$'
+
     # E24 class resistor values
-    __e24 =( 1.0 , 10 ,	100 , 1.0 * 10 ** 3 , 10 * 10 ** 3 , 100 * 10 ** 3 , 1.0 * 10 ** 6,
+    __E24 =( 1.0 , 10 ,	100 , 1.0 * 10 ** 3 , 10 * 10 ** 3 , 100 * 10 ** 3 , 1.0 * 10 ** 6,
              1.1 , 11 ,	110 , 1.1 * 10 ** 3 , 11 * 10 ** 3 , 110 * 10 ** 3 , 1.1 * 10 ** 6,
              1.2 , 12 ,	120 , 1.2 * 10 ** 3 , 12 * 10 ** 3 , 120 * 10 ** 3 , 1.2 * 10 ** 6,
              1.3 , 13 ,	130 , 1.3 * 10 ** 3 , 13 * 10 ** 3 , 130 * 10 ** 3 , 1.3 * 10 ** 6,
@@ -214,7 +134,11 @@ class resistor(electricComponent):
 
     @staticmethod
     def e24():
-        return resistor.__e24
+        return resistor.__E24
+
+    @staticmethod
+    def unit():
+        return resistor.__UNIT
 
     @staticmethod
     def series(*args):
