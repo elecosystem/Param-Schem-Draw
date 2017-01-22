f343b265dfc8214819d54f898efd42e0f548c1c3
Pedro Martins <martinspedro@ua.pt>
2017-01-22 23:28:51 +0000

    DC_cicuits now supports values on/off | Changed filename of DC_001 to test

--- DC_circuits.py
+++ DC_circuits.py
@@ -23,7 +23,11 @@ extension = '.png'
 ################################################################################
 # Circuit Example
 
-def DC_001(params, ekey):
+# TODO
+# change params to **kwargs ??
+# modify classes to support location of label.
+#
+def example_01(params, ekey, label=True):
     # Parameters
     V1 = vSource(params['$V_1$'], '$V_1$')
     V2 = vSource(params['$V_2$'], '$V_2$')
@@ -38,46 +42,49 @@ def DC_001(params, ekey):
 
     # add the first voltage source, his name, the first resistor and a dot
     V1.schem = d.add( e.SOURCE_V, label= V1.label )
-    V1.schem.add_label(V1.value, loc='bot')
     R1.schem = d.add( e.RES, d='right', label= R1.label )
-    R1.schem.add_label(R1.value, loc='bot')
+    R1.schem.add_label(R1.resistance, loc='bot')
     d.add( e.DOT )
 
     #save node position to stack, add vertical resistor, a dot and pop stack (point draw start point to the node)
     d.push()
     R2.schem = d.add( e.RES, d='down', label= R2.label )
-    R2.schem.add_label(R2.value, loc='bot')
     d.add( e.DOT )
     d.pop()
 
     # add resistor, a dota and save node
     R3.schem = d.add( e.RES, d='right', label= R3.label )
-    R3.schem.add_label(R3.value, loc='bot')
     d.add( e.DOT )
     d.push()
 
     # draw 2nd voltage source, add his name,a dot and return to the start node
     # the source must be drawed down, so if one wants the voltage drop from up to down, the source must be reversed
     V2.schem = d.add( e.SOURCE_V, d='down', reverse='True', label= V2.label )
-    V2.schem.add_label(V2.value, loc='bot')
     d.add( e.DOT )
     d.pop()
 
     # add a line to the right, then a resistor
     d.add( e.LINE, d='right', l=2.5)
     R4.schem = d.add( e.RES, d='down', label= R4.label )
-    R4.schem.add_label(R4.value, loc='bot')
 
     # Link all the ground nodes, add a dot to V1 and a ground signal
     d.add( e.LINE, to=V1.schem.start )
     d.add( e.DOT )
     d.add( e.GND )
 
+    # Add labels if the circuit requires parametrization
+    if label:
+        V1.schem.add_label(V1.voltage, loc='bot')
+        R2.schem.add_label(R2.resistance, loc='bot')
+        R3.schem.add_label(R3.resistance, loc='bot')
+        V2.schem.add_label(V2.resistance, loc='bot')
+        R4.schem.add_label(R4.resistance, loc='bot')
+
     # draw the circuit, but don't show
     d.draw(showplot = False)
 
     # save schematic (full path + Circuit ID + parametrize identifier + extension)
-    filename = 'DC_001_' + str(ekey) + extension
+    filename = 'test_01' + str(ekey) + extension
     d.save(path + filename)
 
     # close all open plots
