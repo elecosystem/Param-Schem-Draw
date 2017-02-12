a1c2281d18a3857c01d910c2c65fc3d41471a9c2
Pedro Martins <martinspedro@ua.pt>
2017-02-12 00:44:22 +0000

    New Circuit - DC_001

--- DC_circuits.py
+++ DC_circuits.py
@@ -5,16 +5,14 @@ import matplotlib
 matplotlib.use('Agg')
 
 #Documentation:  http://cdelker.bitbucket.org/SchemDraw.html
-# import SchemDraw
 import SchemDraw as schem
 import SchemDraw.elements as e
 
 # Import library to parametrize the used of SchemDraw
-from ParamSchemDraw import electricComponent, resistor, vSource
+from ParamSchemDraw import electricComponent, resistor, vSource, iSource
 
 # Folder to save the schematics in server
 path = '/projects/15860edd-0fa5-4a0a-820c-2bb86b4c0cd5/ENUNCIADOS/IMAGENS/'
-
 # Image extension
 extension = '.png'
 
@@ -22,14 +20,14 @@ extension = '.png'
 # Circuit Example
 
 # TODO
-# change params to **kwargs 
-# modify classes to support location of label.
+# change params to **kwargs
+# modify classes to support location of label. ? Is it really necessary?
 
 def example_01(params, ekey, label=True):
     # Parameters
     V1 = vSource(params['$V_1$'], '$V_1$')
     V2 = vSource(params['$V_2$'], '$V_2$')
-    
+
     R1 = resistor(params['$R_1$'], '$R_1$')
     R2 = resistor(params['$R_2$'], '$R_2$')
     R3 = resistor(params['$R_3$'], '$R_3$')
@@ -86,6 +84,48 @@ def example_01(params, ekey, label=True):
     d.save(path + filename)
 
     # close all open plots
+    matplotlib.pyplot.close('all')
+
+    return filename
+
+def DC_001(params, ekey, label=True):
+    V1 = vSource(params['$V_1$'], '$V_1$')
+    V2 = vSource(params['$V_2$'], '$V_2$')
+    I1 = vSource(params['$I_1$'], '$I_1$')
+    I2 = vSource(params['$I_2$'], '$I_2$')
+    I3 = vSource(params['$I_1$'], '$I_3$')
+
+    d = schem.Drawing()
+    I1.schem = d.add( e.SOURCE_I , reverse='True', label=I1.label)
+    V1.schem = d.add( e.SOURCE_V, d='right', label= V1.label )
+    d.push()
+    d.add( e.DOT )
+    V2.schem = d.add( e.SOURCE_V, d='down', reverse='True', label= V2.label )
+    d.pop()
+    d.add( e.LINE, d='right', l=2.5)
+    d.push()
+    I2.schem = d.add( e.SOURCE_I, d='down', label= I2.label )
+    d.pop()
+    d.add( e.LINE, d='right', l=2.5)
+    d.push()
+    I3.schem = d.add( e.SOURCE_I, d='down', reverse='True', label= I3.label )
+    d.add( e.LINE, to=I1.schem.end )
+    d.add( e.DOT )
+    d.add( e.GND )
+
+    if label:
+        V1.schem.add_label(V1.voltageEng, loc='bot')
+        V2.schem.add_label(V2.voltageEng, loc='bot')
+        I1.schem.add_label(I1.currentEng, loc='bot')
+        I2.schem.add_label(I2.currentEng, loc='bot')
+        I3.schem.add_label(I3.currentEng, loc='bot')
+
+    d.draw(showplot = False)
+
+    # save schematic (full path + Circuit ID + parametrize identifier + extension)
+    filename = 'test_0_' + str(ekey) + extension
+    d.save(path + filename)
+
     matplotlib.pyplot.close('all')
 
     return filename
